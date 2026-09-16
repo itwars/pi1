eb555146595be8aef739d92e9e27d98231af2b38
itwars <vincent.rabah@gmail.com>
2026-09-16 08:58:42 +0200

    feat download

---
+++ download.sh
@@ -0,0 +1,3 @@
+hf download inclusionAI/Ling-3.0-tiny-GGUF    --include "*Q4_K_M.gguf"                --local-dir ~/models/fast
+hf download ornith-ai/Ornith-1.5-35B-A3B-GGUF --include "mmproj-*.gguf"               --local-dir ~/models/reasoning
+hf download ornith-ai/Ornith-1.5-35B-A3B-GGUF --include "Ornith-1.5-35B-Q4_K_M.gguf"  --local-dir ~/models/reasoning
